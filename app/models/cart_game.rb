ff026dcc01bf8b4723fbe768c862aaec86b551d5
David Swift <davidswift@Davids-Air.home>
2017-02-03 22:06:48 +0000

    More styling

--- app/models/cart_game.rb
+++ app/models/cart_game.rb
@@ -2,4 +2,8 @@ class CartGame < ApplicationRecord
   belongs_to :cart
   belongs_to :game, optional: true
   belongs_to :order, optional: true
+
+  def total_price
+    game.price * quantity
+  end
 end
