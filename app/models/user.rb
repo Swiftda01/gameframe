3ffac85c4b7f7cbba37b0cc4cfd46e8108caf130
David Swift <davidswift@Davids-Air.home>
2017-04-15 15:54:46 +0100

    Minor change to stylesheet and fixed spelling error in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :date_of_birth, presence: true
-  validate :varify_age
+  validate :verify_age
 
   private
   
-  def varify_age
+  def verify_age
     unless self.date_of_birth.nil?
       if (self.date_of_birth.to_date + 16.years) > Date.today
         self.errors.add(:date_of_birth, "- You must be at least 16 years old to use this service")
