8597399e6f07d383c760bd1dedf9c1bb4f9bad3c
David Swift <davidswift@Davids-Air.home>
2017-05-07 15:09:56 +0100

    Created relationships between games and genres

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,7 @@
 class Game < ApplicationRecord
   has_many :cart_games, dependent: :destroy
+  has_many :game_genres
+  has_many :genres, through: :game_genres
 
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 
